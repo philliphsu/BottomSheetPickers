a7b75a1d04d793dc7e6146277c9303022cbcc3ac
Phillip Hsu <philliphsu@outlook.com>
2016-10-02 16:01:45 -0700

    Create test DialogFragment class

---
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/time/CalculatorGridTimePickerDialog.java
@@ -0,0 +1,14 @@
+package com.philliphsu.bottomsheetpickers.time;
+
+import com.philliphsu.bottomsheetpickers.R;
+
+/**
+ * Created by Phillip Hsu on 10/2/2016.
+ */
+public class CalculatorGridTimePickerDialog extends BottomSheetTimePickerDialog {
+
+    @Override
+    int contentLayout() {
+        return R.layout.calculator_dialog_grid_time_picker;
+    }
+}
