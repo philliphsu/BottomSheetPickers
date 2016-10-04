16d76b94e0838c4b823336400f3e6716be679b81
Phillip Hsu <philliphsu@outlook.com>
2016-10-04 12:54:22 -0700

    Delete test dialog class

--- sample/src/main/java/com/example/bottomsheetpickers/MainActivity.java
+++ sample/src/main/java/com/example/bottomsheetpickers/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.philliphsu.bottomsheetpickers.time.BottomSheetTimePickerDialog;
-import com.philliphsu.bottomsheetpickers.time.CalculatorGridTimePickerDialog;
 import com.philliphsu.bottomsheetpickers.time.grid.GridTimePickerDialog;
 import com.philliphsu.bottomsheetpickers.time.keypad.NumpadTimePickerDialog;
 
@@ -42,9 +41,6 @@ public class MainActivity extends AppCompatActivity implements BottomSheetTimePi
                     GridTimePickerDialog dialog = GridTimePickerDialog.newInstance(
                             MainActivity.this, 0, 0, DateFormat.is24HourFormat(MainActivity.this));
                     dialog.setThemeDark(checkedId == R.id.choice_grid_selector_dark);
-                    dialog.show(getSupportFragmentManager(), TAG);
-                } else {
-                    CalculatorGridTimePickerDialog dialog = new CalculatorGridTimePickerDialog();
                     dialog.show(getSupportFragmentManager(), TAG);
                 }
             }
