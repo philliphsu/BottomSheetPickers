a6f871812b3091727c2bf21bbe831871026b9919
Phillip Hsu <philliphsu@outlook.com>
2016-10-04 02:10:56 -0700

    Deprecate test dialog

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/time/CalculatorGridTimePickerDialog.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/time/CalculatorGridTimePickerDialog.java
@@ -2,13 +2,12 @@ package com.philliphsu.bottomsheetpickers.time;
 
 import com.philliphsu.bottomsheetpickers.R;
 
-/**
- * Created by Phillip Hsu on 10/2/2016.
- */
+// TODO: Delete this class
+@Deprecated
 public class CalculatorGridTimePickerDialog extends BottomSheetTimePickerDialog {
 
     @Override
-    int contentLayout() {
+    protected int contentLayout() {
         return R.layout.calculator_dialog_grid_time_picker;
     }
 }
