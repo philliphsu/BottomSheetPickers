a9431680839b61f82f19f5135acab3901ccc0d18
Phillip Hsu <philliphsu@outlook.com>
2016-10-29 22:33:11 -0700

    Use bold typeface for selected year indicator

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/date/TextViewWithHighlightIndicator.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/date/TextViewWithHighlightIndicator.java
@@ -3,6 +3,8 @@ package com.philliphsu.bottomsheetpickers.date;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 
@@ -14,6 +16,19 @@ import com.philliphsu.bottomsheetpickers.Utils;
  * YearPickerView}, that matches the style in the Material Design spec.
  */
 public class TextViewWithHighlightIndicator extends TextViewWithIndicator {
+
+    // --------------------------------------------------------------------------------------------
+    // TODO: Consider moving this to Utils so other classes may make use of the light typeface.
+    private static final Typeface HIGHLIGHT_TYPEFACE;
+
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            HIGHLIGHT_TYPEFACE = Typeface.create("sans-serif-light", Typeface.BOLD);
+        } else {
+            HIGHLIGHT_TYPEFACE = Typeface.DEFAULT_BOLD;
+        }
+    }
+    // --------------------------------------------------------------------------------------------
 
     private final int    mHighlightColor;
     private final String mItemIsSelectedText;
@@ -40,6 +55,7 @@ public class TextViewWithHighlightIndicator extends TextViewWithIndicator {
         super.onDraw(canvas);
         setTextColor(mDrawHighlight ? mHighlightColor : mDefaultTextColor);
         setTextSize(TypedValue.COMPLEX_UNIT_PX, mDrawHighlight ? mSelectedTextSize : mDefaultTextSize);
+        setTypeface(mDrawHighlight ? HIGHLIGHT_TYPEFACE : Typeface.DEFAULT);
     }
 
     @Override
