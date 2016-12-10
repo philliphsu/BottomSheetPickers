15eb25df585c37e00938f98fbcb8ed5797f991bf
Phillip Hsu <philliphsu@outlook.com>
2016-12-09 16:16:37 -0800

    Use fade animations for MonthPickerView

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/date/DayPickerViewAnimator.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/date/DayPickerViewAnimator.java
@@ -3,7 +3,10 @@ package com.philliphsu.bottomsheetpickers.date;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ViewAnimator;
+
+import com.philliphsu.bottomsheetpickers.R;
 
 import static com.philliphsu.bottomsheetpickers.date.PagingDayPickerView.DAY_PICKER_INDEX;
 import static com.philliphsu.bottomsheetpickers.date.PagingDayPickerView.MONTH_PICKER_INDEX;
@@ -13,28 +16,37 @@ import static com.philliphsu.bottomsheetpickers.date.PagingDayPickerView.MONTH_P
  */
 final class DayPickerViewAnimator extends ViewAnimator {
 
-    private final Animation mInAnimation;
-    private final Animation mOutAnimation;
+    /** Used when the PagingDayPickerView slides into view. */
+    private final Animation mSlideUpInAnimation;
+
+    /** Used when the PagingDayPickerView slides out of view. */
+    private final Animation mSlideDownOutAnimation;
+
+    /** Used when the MonthPickerView fades into view. */
+    private final Animation mFadeInAnimation;
+
+    /** Used when the MonthDayPickerView fades out of view. */
+    private final Animation mFadeOutAnimation;
 
     public DayPickerViewAnimator(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mInAnimation = getInAnimation();
-        mOutAnimation = getOutAnimation();
+        mSlideDownOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_down);
+        mSlideUpInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_up);
+        mFadeInAnimation = AnimationUtils.loadAnimation(context, R.anim.fade_in);
+        mFadeOutAnimation = AnimationUtils.loadAnimation(context, R.anim.fade_out);
     }
 
     @Override
     public void setDisplayedChild(int whichChild) {
-        if (mInAnimation != null && mOutAnimation != null) {
-            switch (whichChild) {
-                case DAY_PICKER_INDEX:
-                    setInAnimation(mInAnimation);
-                    setOutAnimation(null);
-                    break;
-                case MONTH_PICKER_INDEX:
-                    setInAnimation(null);
-                    setOutAnimation(mOutAnimation);
-                    break;
-            }
+        switch (whichChild) {
+            case DAY_PICKER_INDEX:
+                setInAnimation(mSlideUpInAnimation);
+                setOutAnimation(mFadeOutAnimation);
+                break;
+            case MONTH_PICKER_INDEX:
+                setInAnimation(mFadeInAnimation);
+                setOutAnimation(mSlideDownOutAnimation);
+                break;
         }
         super.setDisplayedChild(whichChild);
     }
