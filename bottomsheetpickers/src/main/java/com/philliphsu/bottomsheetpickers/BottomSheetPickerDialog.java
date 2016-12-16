885262052d4023fbea412e1c97ee861fb3b1c68e
Phillip Hsu <philliphsu@outlook.com>
2016-12-16 15:42:32 -0800

    Optimize dialog width for larger form factors

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/BottomSheetPickerDialog.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/BottomSheetPickerDialog.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -64,7 +63,7 @@ public abstract class BottomSheetPickerDialog extends BottomSheetDialogFragment 
     @NonNull
     @Override
     public final Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new BottomSheetDialog(getContext(), R.style.BottomSheetDialogTheme);
+        return new CustomWidthBottomSheetDialog(getContext(), R.style.BottomSheetDialogTheme);
     }
 
     // TODO: Time picker subclasses need to call up to super!
---
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/CustomWidthBottomSheetDialog.java
@@ -0,0 +1,22 @@
+package com.philliphsu.bottomsheetpickers;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.support.design.widget.BottomSheetDialog;
+import android.view.ViewGroup;
+
+final class CustomWidthBottomSheetDialog extends BottomSheetDialog {
+    public CustomWidthBottomSheetDialog(@NonNull Context context, @StyleRes int theme) {
+        super(context, theme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int width = getContext().getResources().getDimensionPixelSize(R.dimen.bottom_sheet_width);
+        getWindow().setLayout(width > 0 ? width : ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+}
