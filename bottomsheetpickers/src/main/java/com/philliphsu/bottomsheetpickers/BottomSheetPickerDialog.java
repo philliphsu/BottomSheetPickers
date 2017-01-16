311a6c60797eef63b8bbf70c059ed14b6a2350e1
Phillip Hsu <philliphsu@outlook.com>
2017-01-15 16:59:44 -0800

    Added new APIs setAccentColor and setBackgroundColor

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/BottomSheetPickerDialog.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/BottomSheetPickerDialog.java
@@ -3,6 +3,7 @@ package com.philliphsu.bottomsheetpickers;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -23,15 +24,24 @@ public abstract class BottomSheetPickerDialog extends BottomSheetDialogFragment 
     protected static final String KEY_DARK_THEME = "dark_theme";
     protected static final String KEY_THEME_SET_AT_RUNTIME = "theme_set_at_runtime";
 
+    private static final String KEY_ACCENT_COLOR = "accent_color";
+    private static final String KEY_ACCENT_COLOR_SET_AT_RUNTIME = "accent_color_set_at_runtime";
+    private static final String KEY_BACKGROUND_COLOR = "background_color";
+    private static final String KEY_BACKGROUND_COLOR_SET_AT_RUNTIME = "background_color_set_at_runtime";
+
     // TODO: Other classes in this package may need this? If not, make private.
     // TODO: Remove duplicates in time picker classes.
     protected boolean mThemeDark;
     protected boolean mThemeSetAtRuntime;
+    protected boolean mAccentColorSetAtRuntime;
+    protected boolean mBackgroundColorSetAtRuntime;
 
     // TODO: Remove retrievals of these values in time picker classes.
     protected int mDarkGray;
     protected int mLightGray;
+    protected int mWhite;
     protected int mAccentColor;
+    protected int mBackgroundColor;
 
     @LayoutRes
     protected abstract int contentLayout();
@@ -42,6 +52,10 @@ public abstract class BottomSheetPickerDialog extends BottomSheetDialogFragment 
         if (savedInstanceState != null) {
             mThemeDark = savedInstanceState.getBoolean(KEY_DARK_THEME);
             mThemeSetAtRuntime = savedInstanceState.getBoolean(KEY_THEME_SET_AT_RUNTIME);
+            mAccentColor = savedInstanceState.getInt(KEY_ACCENT_COLOR);
+            mAccentColorSetAtRuntime = savedInstanceState.getBoolean(KEY_ACCENT_COLOR_SET_AT_RUNTIME);
+            mBackgroundColor = savedInstanceState.getInt(KEY_BACKGROUND_COLOR);
+            mBackgroundColorSetAtRuntime = savedInstanceState.getBoolean(KEY_BACKGROUND_COLOR_SET_AT_RUNTIME);
         }
     }
 
@@ -55,14 +69,27 @@ public abstract class BottomSheetPickerDialog extends BottomSheetDialogFragment 
         final Context ctx = getActivity();
         mDarkGray = getColor(ctx, R.color.dark_gray);
         mLightGray = getColor(ctx, R.color.light_gray);
-        mAccentColor = Utils.getThemeAccentColor(getActivity());
+        mWhite = getColor(ctx, android.R.color.white);
+        if (!mAccentColorSetAtRuntime) {
+            mAccentColor = Utils.getThemeAccentColor(getActivity());
+        }
+        if (!mBackgroundColorSetAtRuntime) {
+            mBackgroundColor = mThemeDark? mWhite : mDarkGray;
+        }
 
-        return inflater.inflate(contentLayout(), container, false);
+        View view = inflater.inflate(contentLayout(), container, false);
+        // Set background color of entire view
+        // TODO: Remove setting of this in subclasses.
+        view.setBackgroundColor(mBackgroundColor);
+
+        return view;
     }
 
     @NonNull
     @Override
     public final Dialog onCreateDialog(Bundle savedInstanceState) {
+        // TODO: Pass in a dark/light BottomSheetDialog theme depending on the theme set.
+        // Verify this changes colors for text, selectableBackground, etc. appropriately.
         return new CustomWidthBottomSheetDialog(getContext(), R.style.BottomSheetDialogTheme);
     }
 
@@ -86,5 +113,20 @@ public abstract class BottomSheetPickerDialog extends BottomSheetDialogFragment 
     // TODO: Delete time picker subclasses' implementation.
     public boolean isThemeDark() {
         return mThemeDark;
+    }
+
+
+    public final void setAccentColor(@ColorInt int color) {
+        mAccentColor = color;
+        mAccentColorSetAtRuntime = true;
+    }
+
+    /**
+     * Set the background color. If this color is dark, consider calling
+     * {@link #setThemeDark(boolean)} to ensure text in the picker is visible.
+     */
+    public final void setBackgroundColor(@ColorInt int color) {
+        mBackgroundColor = color;
+        mBackgroundColorSetAtRuntime = true;
     }
 }
