430cb920cd0b343a9f3d2a4a99e0948172b83859
Phillip Hsu <philliphsu@outlook.com>
2017-04-21 00:33:44 -0700

    Add test to verify enabled states of views when time picker is empty initialized.

--- bottomsheetpickers/src/test/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerPresenterTest.java
+++ bottomsheetpickers/src/test/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerPresenterTest.java
@@ -5,8 +5,9 @@ import com.philliphsu.bottomsheetpickers.view.LocaleModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
 
 public class NumberPadTimePickerPresenterTest {
 
@@ -17,6 +18,7 @@ public class NumberPadTimePickerPresenterTest {
     private LocaleModel mLocaleModel;
 
     private NumberPadTimePickerPresenter mPresenter;
+    private boolean mIs24HourMode;
 
     @Before
     public void setupPresenter() {
@@ -24,12 +26,28 @@ public class NumberPadTimePickerPresenterTest {
         // inject the mocks in the test the initMocks method needs to be called.
         MockitoAnnotations.initMocks(this);
 
-        mPresenter = new NumberPadTimePickerPresenter(mView, mLocaleModel, false);
+        mIs24HourMode = false;
+        mPresenter = new NumberPadTimePickerPresenter(mView, mLocaleModel, mIs24HourMode);
+    }
+
+    @Test
+    public void verifyViewEnabledStatesForEmptyState() {
+        mPresenter.onCreate(NumberPadTimePickerState.EMPTY);
+        verify(mView).setNumberKeysEnabled(mIs24HourMode ? 0 : 1, 10);
+        verify(mView).setBackspaceEnabled(false);
+        verify(mView).setAmPmDisplayVisible(!mIs24HourMode);
+        if (!mIs24HourMode) {
+            verify(mView).setAmPmDisplayIndex(mLocaleModel.isAmPmWrittenBeforeTime() ? 0 : 1);
+        }
+        /* Verify setLeftAltKeyText() and setRightAltKeyText() manually. */
+        verify(mView).setLeftAltKeyEnabled(false);
+        verify(mView).setRightAltKeyEnabled(false);
+        verify(mView).setHeaderDisplayFocused(true);
     }
 
     @Test
     public void clickOnNumberKey_UpdatesTimeDisplay() {
         mPresenter.onNumberKeyClick("1");
-        Mockito.verify(mView).updateTimeDisplay("1");
+        verify(mView).updateTimeDisplay("1");
     }
 }
