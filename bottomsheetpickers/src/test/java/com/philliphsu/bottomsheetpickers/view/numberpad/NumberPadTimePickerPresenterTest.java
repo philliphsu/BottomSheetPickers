927b3fe84175071264f7bb57160abdee41ad4700
Phillip Hsu <philliphsu@outlook.com>
2017-04-22 16:16:28 -0700

    Test both 12-hour mode and 24-hour mode simultaneously.

--- bottomsheetpickers/src/test/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerPresenterTest.java
+++ bottomsheetpickers/src/test/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerPresenterTest.java
@@ -7,47 +7,69 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class NumberPadTimePickerPresenterTest {
+    private static final int MODE_12HR = 0;
+    private static final int MODE_24HR = 1;
 
-    @Mock
-    private INumberPadTimePicker.View mView;
+    private final INumberPadTimePicker.View[] mViews = new INumberPadTimePicker.View[2];
+    private final INumberPadTimePicker.Presenter[] mPresenters = new INumberPadTimePicker.Presenter[2];
+
+    {
+        mViews[MODE_12HR] = mock(INumberPadTimePicker.View.class);
+        mViews[MODE_24HR] = mock(INumberPadTimePicker.View.class);
+    }
 
     @Mock
     private LocaleModel mLocaleModel;
 
-    private NumberPadTimePickerPresenter mPresenter;
-    private boolean mIs24HourMode;
-
     @Before
-    public void setupPresenter() {
-        // Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
-        // inject the mocks in the test the initMocks method needs to be called.
+    public void setup() {
+        // Inject mocks annotated with the @Mock annotation.
         MockitoAnnotations.initMocks(this);
-
-        mIs24HourMode = false;
-        mPresenter = new NumberPadTimePickerPresenter(mView, mLocaleModel, mIs24HourMode);
+        setupPresenters();
     }
 
     @Test
     public void verifyViewEnabledStatesForEmptyState() {
-        mPresenter.onCreate(NumberPadTimePickerState.EMPTY);
-        verify(mView).setNumberKeysEnabled(mIs24HourMode ? 0 : 1, 10);
-        verify(mView).setBackspaceEnabled(false);
-        verify(mView).setAmPmDisplayVisible(!mIs24HourMode);
-        if (!mIs24HourMode) {
-            verify(mView).setAmPmDisplayIndex(mLocaleModel.isAmPmWrittenBeforeTime() ? 0 : 1);
-        }
+        mPresenters[MODE_12HR].onCreate(NumberPadTimePickerState.EMPTY);
+        verify(mViews[MODE_12HR]).setNumberKeysEnabled(1, 10);
+        verify(mViews[MODE_12HR]).setBackspaceEnabled(false);
+        verify(mViews[MODE_12HR]).setAmPmDisplayVisible(true);
+        verify(mViews[MODE_12HR]).setAmPmDisplayIndex(mLocaleModel.isAmPmWrittenBeforeTime() ? 0 : 1);
         /* Verify setLeftAltKeyText() and setRightAltKeyText() manually. */
-        verify(mView).setLeftAltKeyEnabled(false);
-        verify(mView).setRightAltKeyEnabled(false);
-        verify(mView).setHeaderDisplayFocused(true);
+        verify(mViews[MODE_12HR]).setLeftAltKeyEnabled(false);
+        verify(mViews[MODE_12HR]).setRightAltKeyEnabled(false);
+        verify(mViews[MODE_12HR]).setHeaderDisplayFocused(true);
+
+        mPresenters[MODE_24HR].onCreate(NumberPadTimePickerState.EMPTY);
+        verify(mViews[MODE_24HR]).setNumberKeysEnabled(0, 10);
+        verify(mViews[MODE_24HR]).setBackspaceEnabled(false);
+        verify(mViews[MODE_24HR]).setAmPmDisplayVisible(false);
+        /* Notice setAmPmDisplayIndex() is not called. */
+        /* Verify setLeftAltKeyText() and setRightAltKeyText() manually. */
+        verify(mViews[MODE_24HR]).setLeftAltKeyEnabled(false);
+        verify(mViews[MODE_24HR]).setRightAltKeyEnabled(false);
+        verify(mViews[MODE_24HR]).setHeaderDisplayFocused(true);
     }
 
     @Test
     public void clickOnNumberKey_UpdatesTimeDisplay() {
-        mPresenter.onNumberKeyClick("1");
-        verify(mView).updateTimeDisplay("1");
+        mPresenters[MODE_12HR].onNumberKeyClick("1");
+        verify(mViews[MODE_12HR]).updateTimeDisplay("1");
+    }
+
+    void setupPresenters() {
+        initPresenter(MODE_12HR);
+        initPresenter(MODE_24HR);
+    }
+
+    private void initPresenter(int mode) {
+        if (mPresenters[mode] == null) {
+            mPresenters[mode] = new NumberPadTimePickerPresenter(
+                    mViews[mode], mLocaleModel, mode == MODE_24HR);
+        }
     }
 }
