660759b54469907b6d2f228d19a62eb03b165d3d
Phillip Hsu <philliphsu@outlook.com>
2017-05-02 17:55:52 -0700

    Do not highlight divider.

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePicker.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePicker.java
@@ -18,10 +18,6 @@ class NumberPadTimePicker extends LinearLayout implements INumberPadTimePicker.V
     private final TextView mTimeDisplay;
     private final TextView mAmPmDisplay;
     private final View mBackspace;
-    private final View mDivider;
-
-    private final @ColorInt int mAccentColor;
-    private final @ColorInt int mSecondaryTextColor;
 
     public NumberPadTimePicker(Context context) {
         this(context, null);
@@ -40,10 +36,6 @@ class NumberPadTimePicker extends LinearLayout implements INumberPadTimePicker.V
         mTimeDisplay = (TextView) findViewById(R.id.bsp_input_time);
         mAmPmDisplay = (TextView) findViewById(R.id.bsp_input_ampm);
         mBackspace = findViewById(R.id.bsp_backspace);
-        mDivider = findViewById(R.id.bsp_input_time_divider);
-
-        mAccentColor = colorAccent(context);
-        mSecondaryTextColor = textColorSecondary(context);
     }
 
     @TargetApi(21)
@@ -56,10 +48,6 @@ class NumberPadTimePicker extends LinearLayout implements INumberPadTimePicker.V
         mTimeDisplay = (TextView) findViewById(R.id.bsp_input_time);
         mAmPmDisplay = (TextView) findViewById(R.id.bsp_input_ampm);
         mBackspace = findViewById(R.id.bsp_backspace);
-        mDivider = findViewById(R.id.bsp_input_time_divider);
-
-        mAccentColor = colorAccent(context);
-        mSecondaryTextColor = textColorSecondary(context);
     }
 
     @Override
@@ -117,10 +105,10 @@ class NumberPadTimePicker extends LinearLayout implements INumberPadTimePicker.V
         mNumberPad.setRightAltKeyEnabled(enabled);
     }
 
+    @Deprecated
     @Override
     public void setHeaderDisplayFocused(boolean focused) {
-        // Add or remove the highlight from the divider.
-        mDivider.setBackgroundColor(focused ? mAccentColor: mSecondaryTextColor);
+        // Do nothing.
     }
 
     void setOnBackspaceClickListener(OnClickListener l) {
