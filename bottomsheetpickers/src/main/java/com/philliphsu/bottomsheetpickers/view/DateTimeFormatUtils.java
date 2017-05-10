62d58f91fb0b605e9c7f6f0a3a1897584222bdd3
Phillip Hsu <philliphsu@outlook.com>
2017-05-09 18:06:54 -0700

    Implement solutions for time format utility methods below API level 18.

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/DateTimeFormatUtils.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/DateTimeFormatUtils.java
@@ -1,11 +1,11 @@
 package com.philliphsu.bottomsheetpickers.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.text.format.DateFormat;
+import android.text.format.DateUtils;
 
 import java.util.Locale;
 
@@ -14,47 +14,58 @@ import java.util.Locale;
  */
 final class DateTimeFormatUtils {
     /**
-     * The time separator is defined in the Unicode CLDR and cannot be supposed to be ":".
-     *
-     * See http://unicode.org/cldr/trac/browser/trunk/common/main
-     *
-     * We pass the correct "skeleton" depending on 12 or 24 hours view and then extract the
-     * separator as the character which is just after the hour marker in the returned pattern.
+     * @return The time separator used in the user's locale.
      */
-    @TargetApi(18)
     static String getTimeSeparator(@NonNull Context context, boolean is24Hour) {
-        // TODO: Figure out what to do for API < 18. You may consider formatting a
-        // dummy time string in 24-hour time using Android's DateFormat, then
-        // iterating through the string until you find a non-digit character.
-        // This should be the separator.
-        final String bestDateTimePattern = DateFormat.getBestDateTimePattern(
-                getPrimaryLocale(context), (is24Hour) ? "Hm" : "hm");
-        final String separatorText;
-        // See http://www.unicode.org/reports/tr35/tr35-dates.html for hour formats
-        final char[] hourFormats = {'H', 'h', 'K', 'k'};
-        int hIndex = lastIndexOfAny(bestDateTimePattern, hourFormats);
-        if (hIndex == -1) {
-            // Default case
-            separatorText = ":";
+        // The time separator is defined in the Unicode CLDR and cannot be supposed to be ":".
+        // See http://unicode.org/cldr/trac/browser/trunk/common/main.
+        if (Build.VERSION.SDK_INT >= 18) {
+            // We pass the correct "skeleton" depending on 12 or 24 hours view and then extract the
+            // separator, which is the character just after the hour marker in the returned pattern.
+            final String bestDateTimePattern = DateFormat.getBestDateTimePattern(
+                    getPrimaryLocale(context), (is24Hour) ? "Hm" : "hm");
+            final String separatorText;
+            // See http://www.unicode.org/reports/tr35/tr35-dates.html for hour formats
+            final char[] hourFormats = {'H', 'h', 'K', 'k'};
+            int hIndex = lastIndexOfAny(bestDateTimePattern, hourFormats);
+            if (hIndex == -1) {
+                // Default case
+                separatorText = ":";
+            } else {
+                separatorText = Character.toString(bestDateTimePattern.charAt(hIndex + 1));
+            }
+            return separatorText;
         } else {
-            separatorText = Character.toString(bestDateTimePattern.charAt(hIndex + 1));
+            // Format a dummy time string in 24-hour time, then iterate through the string until
+            // we find a non-digit character.
+            final String formatted24HourTime = DateUtils.formatDateTime(context,
+                    System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_24HOUR);
+            for (int i = 0; i < formatted24HourTime.length(); i++) {
+                final char c = formatted24HourTime.charAt(i);
+                if (!Character.isDigit(c)) {
+                    return Character.toString(c);
+                }
+            }
+            return "";
         }
-        return separatorText;
     }
 
     /**
      * @return {@code true} if the AM or PM label is written before the time
      *         in the user's locale.
      */
-    @TargetApi(18)
     static boolean isAmPmWrittenBeforeTime(@NonNull Context context) {
-        // TODO: Figure out what to do for API < 18. You may consider formatting a
-        // dummy time string in 12-hour time using Android's DateFormat, then
-        // checking if the string starts with a non-digit character. This should
-        // be the AM/PM label.
-        final String dateTimePattern = DateFormat.getBestDateTimePattern(
-                getPrimaryLocale(context), "hm");
-        return dateTimePattern.startsWith("a");
+        if (Build.VERSION.SDK_INT >= 18) {
+            final String dateTimePattern = DateFormat.getBestDateTimePattern(
+                    getPrimaryLocale(context), "hm");
+            return dateTimePattern.startsWith("a");
+        } else {
+            // Format a dummy time string in 12-hour time, then check if the string
+            // starts with a non-digit character. This should be the AM/PM label.
+            final String formatted12HourTime = DateUtils.formatDateTime(context,
+                    System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_12HOUR);
+            return !Character.isDigit(formatted12HourTime.charAt(0));
+        }
     }
 
     private static Locale getPrimaryLocale(@NonNull Context context) {
