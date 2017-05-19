e08bd70cfa567c02cf8b4be67d63eaa1b4006a4b
Phillip Hsu <philliphsu@outlook.com>
2017-05-19 00:56:50 -0700

    Update dimensions for alert dialog layout.

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerDialog.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePickerDialog.java
@@ -2,11 +2,16 @@ package com.philliphsu.bottomsheetpickers.view.numberpad;
 
 import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
 import android.support.v7.app.AlertDialog;
+import android.view.ViewGroup;
+
+import com.philliphsu.bottomsheetpickers.R;
 
 /**
  * Dialog to type in a time.
@@ -43,6 +48,23 @@ public class NumberPadTimePickerDialog extends AlertDialog {
                 onDialogButtonClickListener);
         setButton(BUTTON_NEGATIVE, getContext().getString(android.R.string.cancel),
                 onDialogButtonClickListener);
+
+        // Override the dialog's width if we're running in an eligible layout qualifier.
+        try {
+            // This must be attempted outside of the anonymous inner class,
+            // otherwise we can't catch the exception.
+            final int width = getContext().getResources().getDimensionPixelSize(
+                    R.dimen.bsp_alert_dialog_width);
+            // The dialog's dimensions can only be changed after show() is called.
+            setOnShowListener(new OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    getWindow().setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT);
+                }
+            });
+        } catch (Resources.NotFoundException nfe) {
+            // Do nothing.
+        }
     }
 
     @Override
