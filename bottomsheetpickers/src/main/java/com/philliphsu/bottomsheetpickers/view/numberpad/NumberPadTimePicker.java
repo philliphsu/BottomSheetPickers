fa2ae47934b914b238aa0cdc012fa8d8eeb26644
Phillip Hsu <philliphsu@outlook.com>
2017-05-18 22:22:02 -0700

    Revert alert dialog in landscape mode to use the default layout.

--- bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePicker.java
+++ bottomsheetpickers/src/main/java/com/philliphsu/bottomsheetpickers/view/numberpad/NumberPadTimePicker.java
@@ -83,19 +83,30 @@ class NumberPadTimePicker extends LinearLayout implements INumberPadTimePicker.V
 
     // TODO: Apply the style resource, either the one contained in defStyleAttr or defStyleRes itself.
     private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        switch (context.getResources().getConfiguration().orientation) {
-            case Configuration.ORIENTATION_PORTRAIT:
-            default:
-                setOrientation(VERTICAL);
-                break;
-            case Configuration.ORIENTATION_LANDSCAPE:
-                setOrientation(HORIZONTAL);
-                break;
-        }
         final TypedArray timePickerAttrs = context.obtainStyledAttributes(attrs,
                 R.styleable.BSP_NumberPadTimePicker, defStyleAttr, defStyleRes);
 
         mLayout = retrieveLayout(timePickerAttrs);
+        // Set orientation depending on the requested layout.
+        switch (mLayout) {
+            case LAYOUT_BOTTOM_SHEET:
+                switch (context.getResources().getConfiguration().orientation) {
+                    case Configuration.ORIENTATION_LANDSCAPE:
+                        setOrientation(HORIZONTAL);
+                        break;
+                    case Configuration.ORIENTATION_PORTRAIT:
+                    default:
+                        setOrientation(VERTICAL);
+                        break;
+                }
+                break;
+            // The alert layout has no layout specialized for landscape mode.
+            case LAYOUT_ALERT:
+            default:
+                setOrientation(VERTICAL);
+                break;
+        }
+
         final @LayoutRes int layoutRes = mLayout == LAYOUT_BOTTOM_SHEET
                 ? R.layout.bsp_bottomsheet_numberpad_time_picker
                 : R.layout.bsp_numberpad_time_picker;
